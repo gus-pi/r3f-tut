9e98d9559ed37e054e209dac7b9d4a4bce0491c1
PieMenard <pinedo.m.gustavo@gmail.com>
2026-04-07 19:31:07 -0400

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Canvas } from '@react-three/fiber';
+
+const App = () => {
+    return (
+        <div id="canvas-container" className="w-screen h-screen">
+            <Canvas className="bg-sky-700"></Canvas>
+        </div>
+    );
+};
+export default App;
